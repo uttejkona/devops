4b4bca380e17d2b8209b58a023ab890532ae5de0
Uttej Kona <kvuttej@gmail.com>
2023-07-11 20:02:47 +0530

    Azure Terrafrom file is committed

---
+++ Terraform_Azure/variable.tf
@@ -0,0 +1,20 @@
+variable "rgname" {
+    type = string 
+}
+
+variable "rgloc" {
+    type = string
+    default = "centralindia"
+}
+
+variable "prefix" {
+    type = string
+}
+
+variable "vnet_cidr_prefix" {
+    type = string
+}
+
+variable "subnet_cidr_prefix" {
+    type = string
+}
